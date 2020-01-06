44627c6300ff0a15c7f93f8e0399a86f0e13c278
Matt Russo <matthew.russo91@gmail.com>
2020-01-05 20:59:59 -0500

    #2 attempt (error for n = 23)

--- solutions.js
+++ solutions.js
@@ -10,3 +10,32 @@ function multiplesOf3and5(number) {
   let sum = multiples.reduce((a,b) => a + b, 0);
   return sum;
 }
+
+// Jan 5, 2020
+// 2. 
+function fiboEvenSum(n) {
+  let fibNumbers = []; 
+  if(n === 1){
+    fibNumbers = [0, 1];
+  } else{
+    fibNumbers[0] = 0;
+    fibNumbers[1] = 1;
+    for (let i = 2; i <= n; i++) {
+      // Next fibonacci number = previous + one before previous
+      fibNumbers[i] = fibNumbers[i - 2] + fibNumbers[i - 1];
+    }
+  }
+
+  let evenFibNumbers = [];
+  fibNumbers.map(num => {
+    if(num % 2 === 0){
+      evenFibNumbers.push(num);
+    }
+  });
+
+  let evenFibSum = evenFibNumbers.reduce((a, b) => {
+    return a + b;
+  }, 0);
+
+  return evenFibSum;
+}
